ef23a0401be8aa4a2f2c608668d2c3326ab32e52
JonasJoaquim <72474952+JonasJoaquim@users.noreply.github.com>
2024-04-20 22:41:15 -0300

    Update UsuarioController.java
    Padrão singleton adicionado

--- src/controller/UsuarioController.java
+++ src/controller/UsuarioController.java
@@ -10,18 +10,19 @@ import java.sql.SQLException;
 public class UsuarioController {
     private UsuarioService usuarioService;
     private UsuarioView usuarioView;
+    private static UsuarioController instance; //cria a instancia desse controler
 
-    public UsuarioController() {
+    private UsuarioController() { //privado, para construir em outro lugar do codigo so usando o get.instance
         this.usuarioService = new UsuarioService();
         this.usuarioView = new UsuarioView();
     }
 
-    public void executar() {
+    protected void executar() {
         adicionarUsuario();
         mostrarUsuarios();
     }
 
-    private void adicionarUsuario() {
+    protected void adicionarUsuario() {
         try {
             Usuario novoUsuario = usuarioView.obterNovoUsuario();
             ValidacaoController.validarLogin(novoUsuario.getLogin());
@@ -36,11 +37,22 @@ public class UsuarioController {
         }
     }
 
-    private void mostrarUsuarios() {
+    protected void mostrarUsuarios() {
         try {
             usuarioView.mostrarUsuarios(usuarioService.getUsuarios());
         } catch (IOException | ClassNotFoundException | SQLException e) {
             System.out.println("Erro ao obter usuários: " + e.getMessage());
         }
     }
+
+    public static UsuarioController getInstance() {
+            if (instance == null) {
+                instance = new UsuarioController();
+            }
+            return instance;
+      /* esse erro aqui gerou automaticamente pelo visual code acho que o tratamento de erro n ta certo 
+       throw new UnsupportedOperationException("Unimplemented method 'getInstance'");
+       */ 
+    }
+    
 }
