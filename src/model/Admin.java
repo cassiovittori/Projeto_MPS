ed15ca05fcb6ee22e7de4044a253380e03c9f2fe
JonasJoaquim <72474952+JonasJoaquim@users.noreply.github.com>
2024-04-25 06:41:37 -0300

    Tipos temporarios de usuarios

---
+++ src/model/Admin.java
@@ -0,0 +1,10 @@
+package model;
+
+public class Admin extends Usuario {
+
+    public Admin(String login, String senha) {
+        super(login, senha);
+        //TODO Auto-generated constructor stub
+    }
+    
+}
