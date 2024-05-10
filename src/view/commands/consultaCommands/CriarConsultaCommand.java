8bdb0e2be88e0e86826e3ea56153280bc73dc901
Cassio Vittori <cassiovittori>
2024-05-10 14:57:07 -0300

    Memento para desfazer uma atualização

--- src/view/commands/consultaCommands/CriarConsultaCommand.java
+++ src/view/commands/consultaCommands/CriarConsultaCommand.java
@@ -3,17 +3,12 @@ package view.commands.consultaCommands;
 import controller.Facade;
 import view.commands.Command;
 
-import java.util.Stack;
-
 public class CriarConsultaCommand implements Command {
     private final Facade fachada;
     private final String crm;
     private final String cpf;
     private final String data;
     private final String motivo;
-
-    private Stack<ConsultaMemento> history = new Stack<>();
-
     public CriarConsultaCommand(String crm, String cpf, String data, String motivo) {
         this.fachada = Facade.getInstance();
         this.crm = crm;
@@ -25,43 +20,6 @@ public class CriarConsultaCommand implements Command {
 
     @Override
     public void executar() {
-        salvarEstado();
-
         fachada.adicionarNovaConsultaCtrl(crm,cpf,data,motivo);
-    }
-    private void salvarEstado() {
-        ConsultaMemento memento = new ConsultaMemento(crm, cpf, data, motivo);
-        history.push(memento);
-    }
-    public void desfazer(){
-        if(!history.isEmpty()){
-            ConsultaMemento ultimoEstado = history.pop();
-        }
-    }
-    privatic static class ConsultaMemento {
-        private final String crm;
-        private final String cpf;
-        private final String data;
-        private final String motivo;
-
-        public ConsultaMemento(String crm, String cpf, String data, String motivo) {
-            this.crm = crm;
-            this.cpf = cpf;
-            this.data = data;
-            this.motivo = motivo;
-        }
-
-        public String getCrm() {
-            return crm;
-        }
-        public String getCpf() {
-            return cpf;
-        }
-        public String getData() {
-            return data;
-        }
-        public String getMotivo() {
-            return motivo;
-        }
     }
 }
--- src/view/commands/usuarioCommands/CriarUsuarioCommand.java
+++ src/view/commands/usuarioCommands/CriarUsuarioCommand.java
@@ -7,22 +7,20 @@ import view.commands.Command;
 
 import java.io.IOException;
 import java.sql.SQLDataException;
-import java.util.Stack;
 
 public class CriarUsuarioCommand implements Command {
-    private final Facade fachada;
-    private final String login;
-    private final String senha;
-    private final String nome;
-    private final String cpf;
-    private final String email;
-    private final String sexo;
-    private final String numContato;
-    private final String dataNascimento;
-    private final String strTipoUsuario;
-    private final String crm;
+    Facade fachada;
+    String login;
+    String senha;
+    String nome;
+    String cpf;
+    String email;
+    String sexo;
+    String numContato;
+    String dataNascimento;
+    String strTipoUsuario;
+    String crm;
 
-    private Stack<UsuarioMemento> history = new Stack<>();
 
     public CriarUsuarioCommand(String login, String senha, String nome, String cpf, String email, String sexo, String numContato, String dataNascimento, String strTipoUsuario, String crm) {
         this.fachada = Facade.getInstance();
@@ -40,92 +38,6 @@ public class CriarUsuarioCommand implements Command {
 
     @Override
     public void executar() {
-        salvarEstado();
         fachada.adicionarNovoUsuarioCtrl(login, senha, nome, cpf, email, sexo, numContato, dataNascimento, strTipoUsuario, crm);
-    }
-
-    private void salvarEstado() {
-        UsuarioMemento memento = new UsuarioMemento(login, senha, nome, cpf, email, sexo, numContato, dataNascimento, strTipoUsuario, crm);
-        history.push(memento);
-    }
-    public void desfazer() {
-        if (!history.isEmpty()) {
-            UsuarioMemento ultimoEstado = history.pop();
-
-            this.login = ultimoEstado.getLogin();
-            this.senha = ultimoEstado.getSenha();
-            this.nome = ultimoEstado.getNome();
-            this.cpf = ultimoEstado.getCpf();
-            this.email = ultimoEstado.getEmail();
-            this.sexo = ultimoEstado.getSexo();
-            this.numContato = ultimoEstado.getNumContato();
-            this.dataNascimento = ultimoEstado.getDataNascimento();
-            this.strTipoUsuario = ultimoEstado.getStrTipoUsuario();
-            this.crm = ultimoEstado.getCrm();
-        }
-    }
-    private static class UsuarioMemento {
-        private final String login;
-        private final String senha;
-        private final String nome;
-        private final String cpf;
-        private final String email;
-        private final String sexo;
-        private final String numContato;
-        private final String dataNascimento;
-        private final String strTipoUsuario;
-        private final String crm;
-
-        public UsuarioMemento(String login, String senha, String nome, String cpf, String email, String sexo, String numContato, String dataNascimento, String strTipoUsuario, String crm) {
-            this.login = login;
-            this.senha = senha;
-            this.nome = nome;
-            this.cpf = cpf;
-            this.email = email;
-            this.sexo = sexo;
-            this.numContato = numContato;
-            this.dataNascimento = dataNascimento;
-            this.strTipoUsuario = strTipoUsuario;
-            this.crm = crm;
-        }
-        public String getLogin() {
-            return login;
-        }
-
-        public String getSenha() {
-            return senha;
-        }
-
-        public String getNome() {
-            return nome;
-        }
-
-        public String getCpf() {
-            return cpf;
-        }
-
-        public String getEmail() {
-            return email;
-        }
-
-        public String getSexo() {
-            return sexo;
-        }
-
-        public String getNumContato() {
-            return numContato;
-        }
-
-        public String getDataNascimento() {
-            return dataNascimento;
-        }
-
-        public String getStrTipoUsuario() {
-            return strTipoUsuario;
-        }
-
-        public String getCrm() {
-            return crm;
-        }
     }
 }
