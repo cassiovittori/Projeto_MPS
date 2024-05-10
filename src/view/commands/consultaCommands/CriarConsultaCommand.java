06be49f71895d58357685828a356b06cfb91ce29
Cassio Vittori <cassiovittori>
2024-05-10 14:40:46 -0300

    Implementado Padrão Memento, associado com Command

--- src/view/commands/consultaCommands/CriarConsultaCommand.java
+++ src/view/commands/consultaCommands/CriarConsultaCommand.java
@@ -3,12 +3,17 @@ package view.commands.consultaCommands;
 import controller.Facade;
 import view.commands.Command;
 
+import java.util.Stack;
+
 public class CriarConsultaCommand implements Command {
     private final Facade fachada;
     private final String crm;
     private final String cpf;
     private final String data;
     private final String motivo;
+
+    private Stack<ConsultaMemento> history = new Stack<>();
+
     public CriarConsultaCommand(String crm, String cpf, String data, String motivo) {
         this.fachada = Facade.getInstance();
         this.crm = crm;
@@ -20,6 +25,43 @@ public class CriarConsultaCommand implements Command {
 
     @Override
     public void executar() {
+        salvarEstado();
+
         fachada.adicionarNovaConsultaCtrl(crm,cpf,data,motivo);
+    }
+    private void salvarEstado() {
+        ConsultaMemento memento = new ConsultaMemento(crm, cpf, data, motivo);
+        history.push(memento);
+    }
+    public void desfazer(){
+        if(!history.isEmpty()){
+            ConsultaMemento ultimoEstado = history.pop();
+        }
+    }
+    privatic static class ConsultaMemento {
+        private final String crm;
+        private final String cpf;
+        private final String data;
+        private final String motivo;
+
+        public ConsultaMemento(String crm, String cpf, String data, String motivo) {
+            this.crm = crm;
+            this.cpf = cpf;
+            this.data = data;
+            this.motivo = motivo;
+        }
+
+        public String getCrm() {
+            return crm;
+        }
+        public String getCpf() {
+            return cpf;
+        }
+        public String getData() {
+            return data;
+        }
+        public String getMotivo() {
+            return motivo;
+        }
     }
 }
